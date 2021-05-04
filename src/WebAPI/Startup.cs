14b153ec8a2c0350775dea2ee06bdf3826b4908d
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2021-05-04 18:02:08 +0200

    Configure middleware

--- src/WebAPI/Startup.cs
+++ src/WebAPI/Startup.cs
@@ -1,3 +1,5 @@
+using ApplicationCore;
+using Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,7 +28,8 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddApplicationCore();
+            services.AddInfrastructure(Configuration);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
