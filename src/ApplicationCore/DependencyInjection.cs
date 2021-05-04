8e34784f2d522551546b4dbaf7f0c4f68ac826a1
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2021-05-04 17:03:00 +0200

    Functions added in DependencyInjection.cs

--- src/ApplicationCore/DependencyInjection.cs
+++ src/ApplicationCore/DependencyInjection.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ApplicationCore
 {
-    public class DependencyInjection
+    public static class DependencyInjection
     {
+        public static void AddApplicationCore(this IServiceCollection services)
+        {
+            services.ConfigureInjection();
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+
+        private static void ConfigureInjection(this IServiceCollection services)
+        {
+            
+        }
     }
 }
