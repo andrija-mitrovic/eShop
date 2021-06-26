d51b3a6f4a25ea90eb6114e80615db1229182b34
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-06-26 09:26:05 +0200

    Buyer aggregate added

---
+++ src/ApplicationCore/Entities/BuyerAggregate/Buyer.cs
@@ -0,0 +1,29 @@
+﻿using ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities.BuyerAggregate
+{
+    public class Buyer : BaseEntity, IAggregateRoot
+    {
+        public string IdentityGuid { get; private set; }
+        private List<PaymentMethod> _paymentMethods = new List<PaymentMethod>();
+        public IEnumerable<PaymentMethod> PaymentMethods => _paymentMethods.AsReadOnly();
+
+        private Buyer()
+        {
+            // required by EF
+        }
+
+        public Buyer(string identity) : this()
+        {
+            if (identity == null) throw new ArgumentNullException();
+            if (identity == string.Empty) throw new ArgumentException("Identity cannot be null");
+
+            IdentityGuid = identity;
+        }
+    }
+}
---
+++ src/ApplicationCore/Entities/BuyerAggregate/PaymentMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities.BuyerAggregate
+{
+    public class PaymentMethod : BaseEntity
+    {
+        public string Alias { get; private set; }
+        public string CardId { get; private set; } // actual card data must be stored in a PCI compliant system, like Stripe
+        public string Last4 { get; private set; }
+    }
+}
