0ed6993fb8b3f34880ade7de6b355acacc67d246
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-07-04 12:34:01 +0200

    CatalogType configuration added

---
+++ src/Infrastructure/Data/Config/CatalogTypeConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Config
+{
+    public class CatalogTypeConfiguration : IEntityTypeConfiguration<CatalogType>
+    {
+        public void Configure(EntityTypeBuilder<CatalogType> builder)
+        {
+            builder.HasKey(ci => ci.Id);
+
+            builder.Property(ci => ci.Id)
+               .UseHiLo("catalog_type_hilo")
+               .IsRequired();
+
+            builder.Property(cb => cb.Type)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
