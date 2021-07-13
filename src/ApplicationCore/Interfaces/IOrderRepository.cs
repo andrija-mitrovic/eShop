2da5f3101d7c2ba4e00b72c1d636226fe373f5ea
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-07-13 08:46:37 +0200

    Order repository added

---
+++ src/ApplicationCore/Interfaces/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationCore.Entities.OrderAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IOrderRepository : IAsyncRepository<Order>
+    {
+        Task<Order> GetByIdWithItemsAsync(int id);
+    }
+}
---
+++ src/Infrastructure/Data/Repositories/OrderRepository.cs
@@ -0,0 +1,22 @@
+﻿using ApplicationCore.Entities.OrderAggregate;
+using ApplicationCore.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class OrderRepository : EfRepository<Order>, IOrderRepository
+    {
+        public OrderRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public Task<Order> GetByIdWithItemsAsync(int id)
+        {
+            return _dbContext.Orders
+                .Include(o => o.OrderItems)
+                .Include($"{nameof(Order.OrderItems)}.{nameof(OrderItem.ItemOrdered)}")
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
