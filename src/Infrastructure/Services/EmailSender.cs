35caf180bafaca7ec25b9d9a8024f5262bb1b833
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-07-16 17:51:07 +0200

    EmailSender added

---
+++ src/Infrastructure/Services/EmailSender.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationCore.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
