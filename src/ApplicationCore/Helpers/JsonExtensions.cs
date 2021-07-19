3502da676a979626c72c6a9a7e9f5a13a2945662
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-07-19 17:35:30 +0200

    JsonExtension added

---
+++ src/ApplicationCore/Helpers/JsonExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace ApplicationCore.Helpers
+{
+    public static class JsonExtensions
+    {
+        private static readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        public static T FromJson<T>(this string json)
+            => JsonSerializer.Deserialize<T>(json, _jsonOptions);
+
+        public static string ToJson<T>(this T obj)
+            => JsonSerializer.Serialize(obj, _jsonOptions);
+    }
+}
