08c80413876e110cee092d8cf03b6e2dcd9e4003
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-07-21 16:20:16 +0200

    UriComposer added

---
+++ src/ApplicationCore/Services/UriComposer.cs
@@ -0,0 +1,17 @@
+﻿using ApplicationCore.Interfaces;
+
+namespace ApplicationCore.Services
+{
+    public class UriComposer : IUriComposer
+    {
+        private readonly string _catalogBaseUrl;
+
+        public UriComposer(string catalogBaseUrl)
+        {
+            _catalogBaseUrl = catalogBaseUrl;
+        }
+
+        public string ComposePicUri(string uriTemplate)
+            => uriTemplate.Replace("http://catalogbaseurltobereplaced", _catalogBaseUrl);
+    }
+}
