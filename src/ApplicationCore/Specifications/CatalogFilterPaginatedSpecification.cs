ea52c69c41bae5c25c633b4c4ee062e5b89e69f5
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-07-23 18:57:51 +0200

    Specifications added

---
+++ src/ApplicationCore/Specifications/CatalogFilterPaginatedSpecification.cs
@@ -0,0 +1,22 @@
+﻿using ApplicationCore.Entities;
+using Ardalis.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Specifications
+{
+    public class CatalogFilterPaginatedSpecification : Specification<CatalogItem>
+    {
+        public CatalogFilterPaginatedSpecification(int skip, int take, int? brandId, int? typeId)
+            : base()
+        {
+            Query
+                .Where(i => (!brandId.HasValue || i.CatalogBrandId == brandId) &&
+                (!typeId.HasValue || i.CatalogTypeId == typeId))
+                .Paginate(skip, take);
+        }
+    }
+}
---
+++ src/ApplicationCore/Specifications/CustomerOrdersWithItemsSpecification.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationCore.Entities.OrderAggregate;
+using Ardalis.Specification;
+using System.Linq;
+
+namespace ApplicationCore.Specifications
+{
+    public class CustomerOrdersWithItemsSpecification : Specification<Order>
+    {
+        public CustomerOrdersWithItemsSpecification(string buyerId)
+        {
+            Query.Where(o => o.BuyerId == buyerId)
+                .Include(o => o.OrderItems)
+                .ThenInclude(i => i.ItemOrdered);
+        }
+    }
+}
