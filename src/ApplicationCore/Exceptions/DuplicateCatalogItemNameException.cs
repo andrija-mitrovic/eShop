17d06d4e0ba3a34cbd0ae0fb47de089a914d0756
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-07-24 08:22:10 +0200

    Exceptions added

---
+++ src/ApplicationCore/Exceptions/DuplicateCatalogItemNameException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ApplicationCore.Exceptions
+{
+    public class DuplicateCatalogItemNameException : Exception
+    {
+        public int DuplicateItemId { get; }
+
+        public DuplicateCatalogItemNameException(string message, int duplicateItemId) : base(message)
+        {
+            DuplicateItemId = duplicateItemId;
+        }
+    }
+}
---
+++ src/ApplicationCore/Exceptions/GuardExtensions.cs
@@ -0,0 +1,20 @@
+﻿using ApplicationCore.Entities.BasketAggregate;
+using Ardalis.GuardClauses;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApplicationCore.Exceptions
+{
+    public static class GuardExtensions
+    {
+        public static void NullBasket(this IGuardClause guardClause, int basketId, Basket basket)
+        {
+            if (basket == null) throw new BasketNotFoundException(basketId);
+        }
+
+        public static void EmptyBasketOnCheckout(this IGuardClause guardClause, IReadOnlyCollection<BasketItem> basketItems)
+        {
+            if (!basketItems.Any()) throw new EmptyBasketOnCheckoutException();
+        }
+    }
+}
