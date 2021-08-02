6819c9f7cde51e4619aea7813aafa7aa772e9311
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-08-02 09:42:16 +0200

    Authorization constants added

--- src/ApplicationCore/Constants/AuthorizationConstants.cs
+++ src/ApplicationCore/Constants/AuthorizationConstants.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ApplicationCore.Constants
+﻿namespace ApplicationCore.Constants
 {
     public class AuthorizationConstants
     {
+        public const string AUTH_KEY = "AuthKeyOfDoomThatMustBeAMinimumNumberOfBytes";
+
+        public const string DEFAULT_PASSWORD = "Pass@word1";
+
+        public const string JWT_SECRET_KEY = "SecretKeyOfDoomThatMustBeAMinimumNumberOfBytes";
+
+        public const string ADMINISTRATORS = "Administrators";
     }
 }
